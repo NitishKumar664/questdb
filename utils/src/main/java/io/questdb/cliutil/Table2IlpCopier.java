55bc41fe73f990d4ed6df0b8195fd90e38e01261
Nitish Kumar <89963100+NitishKumar664@users.noreply.github.com>
2024-05-31 16:14:57 +0530

    Update Table2Ilp Code Readability.java

--- utils/src/main/java/io/questdb/cliutil/Table2IlpCopier.java
+++ utils/src/main/java/io/questdb/cliutil/Table2IlpCopier.java
@@ -1,37 +1,8 @@
-/*******************************************************************************
- *     ___                  _   ____  ____
- *    / _ \ _   _  ___  ___| |_|  _ \| __ )
- *   | | | | | | |/ _ \/ __| __| | | |  _ \
- *   | |_| | |_| |  __/\__ \ |_| |_| | |_) |
- *    \__\_\\__,_|\___||___/\__|____/|____/
- *
- *  Copyright (c) 2014-2019 Appsicle
- *  Copyright (c) 2019-2024 QuestDB
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- ******************************************************************************/
-
-package io.questdb.cliutil;
-
 import io.questdb.client.Sender;
 import io.questdb.griffin.model.IntervalUtils;
 import io.questdb.std.LowerCaseCharSequenceHashSet;
-import io.questdb.std.Numbers;
-import io.questdb.std.NumericException;
 import io.questdb.std.datetime.microtime.MicrosecondClock;
 import io.questdb.std.datetime.microtime.MicrosecondClockImpl;
-import io.questdb.std.datetime.microtime.Timestamps;
 
 import java.sql.*;
 import java.time.temporal.ChronoUnit;
@@ -41,49 +12,31 @@ public class Table2IlpCopier {
 
     public long copyTable(Table2Ilp.Table2IlpParams params) {
         LowerCaseCharSequenceHashSet symbols = createSymbolsSet(params);
-
-        MicrosecondClock microsecondClock = new MicrosecondClockImpl();
-        long totalSentLines = 0;
-        try (Connection connection = getConnection(params.getSourcePgConnectionString())) {
-            connection.setAutoCommit(false);
-            try (PreparedStatement statement = connection.prepareStatement(params.getSourceSelectQuery())) {
-                statement.setFetchSize(8 * 1024);
-                long start = microsecondClock.getTicks();
-
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    ResultSetMetaData metaData = resultSet.getMetaData();
-                    int columnCount = metaData.getColumnCount();
+        MicrosecondClock clock = new MicrosecondClockImpl();
+        long totalLines = 0;
+        try (Connection conn = getConnection(params.getSourcePgConnectionString())) {
+            conn.setAutoCommit(false);
+            try (PreparedStatement stmt = conn.prepareStatement(params.getSourceSelectQuery())) {
+                stmt.setFetchSize(8 * 1024);
+                long start = clock.getTicks();
+                try (ResultSet rs = stmt.executeQuery(); Sender sender = buildSender(params)) {
+                    ResultSetMetaData meta = rs.getMetaData();
+                    int columnCount = meta.getColumnCount();
                     String[] columnNames = new String[columnCount];
                     int[] columnTypes = new int[columnCount];
                     for (int i = 0; i < columnCount; i++) {
-                        columnNames[i] = metaData.getColumnName(i + 1);
-                        columnTypes[i] = metaData.getColumnType(i + 1);
+                        columnNames[i] = meta.getColumnName(i + 1);
+                        columnTypes[i] = meta.getColumnType(i + 1);
                     }
-                    int timestampIndex = getTimestampIndex(columnNames, columnTypes, params.getSourceTimestampColumnName());
-
-                    try (Sender sender = buildLineTcpSender(params)) {
-                        String tableName = params.getDestinationTableName();
-
-                        try {
-                            while (resultSet.next()) {
-                                sendLine(symbols, resultSet, columnCount, columnNames, columnTypes, timestampIndex, sender, tableName);
-                                totalSentLines++;
-
-                                if (totalSentLines % 10000 == 0) {
-                                    long end = microsecondClock.getTicks();
-                                    long linesPerSec = 10000 * Timestamps.SECOND_MICROS / (end - start);
-                                    System.out.println(totalSentLines + " lines, " + linesPerSec + " lines/sec");
-                                    start = microsecondClock.getTicks();
-                                }
-                            }
-                        } catch (Exception th) {
-                            try {
-                                sender.flush();
-                            } catch (Exception flushError) {
-                                System.err.println("Failed to flush sender while handling error");
-                                flushError.printStackTrace();
-                            }
-                            throw th;
+                    int tsIndex = getTimestampIndex(columnNames, columnTypes, params.getSourceTimestampColumnName());
+                    while (rs.next()) {
+                        sendLine(symbols, rs, columnCount, columnNames, columnTypes, tsIndex, sender, params.getDestinationTableName());
+                        totalLines++;
+                        if (totalLines % 10000 == 0) {
+                            long end = clock.getTicks();
+                            long linesPerSec = 10000 * 1_000_000 / (end - start);
+                            System.out.println(totalLines + " lines, " + linesPerSec + " lines/sec");
+                            start = clock.getTicks();
                         }
                     }
                 }
@@ -92,145 +45,121 @@ public class Table2IlpCopier {
             e.printStackTrace();
             System.err.println("Failed to connect to the source");
         } finally {
-            System.out.println("Total sent lines: " + totalSentLines);
+            System.out.println("Total lines sent: " + totalLines);
         }
-        return totalSentLines;
+        return totalLines;
     }
 
-    private static Sender buildLineTcpSender(Table2Ilp.Table2IlpParams params) {
-        Sender.LineSenderBuilder senderBuilder = Sender.builder(Sender.Transport.TCP);
-        senderBuilder.address(params.getDestinationIlpHost() + ":" + params.getDestinationIlpPort());
+    private static Sender buildSender(Table2Ilp.Table2IlpParams params) {
+        Sender.LineSenderBuilder builder = Sender.builder(Sender.Transport.TCP);
+        builder.address(params.getDestinationIlpHost() + ":" + params.getDestinationIlpPort());
         if (params.enableDestinationTls()) {
-            senderBuilder.enableTls();
+            builder.enableTls();
         }
-
         if (params.getDestinationAuthKey() != null) {
-            senderBuilder
-                    .enableAuth(params.getDestinationAuthKey())
-                    .authToken(params.getDestinationAuthToken());
+            builder.enableAuth(params.getDestinationAuthKey())
+                   .authToken(params.getDestinationAuthToken());
         }
-        return senderBuilder.build();
+        return builder.build();
     }
 
     private static LowerCaseCharSequenceHashSet createSymbolsSet(Table2Ilp.Table2IlpParams params) {
         LowerCaseCharSequenceHashSet set = new LowerCaseCharSequenceHashSet();
-        String[] symbols = params.getSymbols();
-        //noinspection ForLoopReplaceableByForEach
-        for (int i = 0, n = symbols.length; i < n; i++) {
-            set.add(symbols[i]);
+        for (String symbol : params.getSymbols()) {
+            set.add(symbol);
         }
         return set;
     }
 
     private static Connection getConnection(String connectionString) throws SQLException {
-        Properties properties = new Properties();
-        properties.setProperty("user", "admin");
-        properties.setProperty("password", "quest");
-        properties.setProperty("sslmode", "disable");
-        properties.setProperty("binaryTransfer", "true");
-        properties.setProperty("preferQueryMode", "extendedForPrepared");
-        return DriverManager.getConnection(connectionString, properties);
+        Properties props = new Properties();
+        props.setProperty("user", "admin");
+        props.setProperty("password", "quest");
+        props.setProperty("sslmode", "disable");
+        props.setProperty("binaryTransfer", "true");
+        props.setProperty("preferQueryMode", "extendedForPrepared");
+        return DriverManager.getConnection(connectionString, props);
     }
 
-    private static long getMicroEpoch(ResultSet resultSet, int timestampIndex) throws SQLException {
-        String ts = resultSet.getString(timestampIndex);
-        long microEpoch;
-        if (ts != null) {
-            try {
-                microEpoch = IntervalUtils.parseFloorPartialTimestamp(ts);
-            } catch (NumericException e) {
-                throw new RuntimeException("Failed to parse designated timestamp: " + ts);
-            }
-            return microEpoch;
-        }
-        return Numbers.LONG_NULL;
+    private static long getMicroEpoch(ResultSet rs, int tsIndex) throws SQLException {
+        String ts = rs.getString(tsIndex);
+        return (ts != null) ? IntervalUtils.parseFloorPartialTimestamp(ts) : Long.MIN_VALUE;
     }
 
-    private static int getTimestampIndex(String[] columnNames, int[] columnTypes, String sourceTimestampColumnName) {
-        sourceTimestampColumnName = sourceTimestampColumnName.toLowerCase();
-        for (int i = 0; i < columnNames.length; i++) {
-            if (columnNames[i].toLowerCase().equals(sourceTimestampColumnName)) {
-                if (columnTypes[i] == Types.TIMESTAMP || columnTypes[i] == Types.DATE) {
-                    return i;
-                } else {
-                    throw new IllegalArgumentException("Source timestamp column '" + sourceTimestampColumnName + "' must be of type TIMESTAMP or DATE but was '" + JDBCType.valueOf(columnTypes[i]).getName() + "'");
-                }
+    private static int getTimestampIndex(String[] colNames, int[] colTypes, String tsColName) {
+        String lcTsColName = tsColName.toLowerCase();
+        for (int i = 0; i < colNames.length; i++) {
+            if (colNames[i].toLowerCase().equals(lcTsColName) && 
+                (colTypes[i] == Types.TIMESTAMP || colTypes[i] == Types.DATE)) {
+                return i;
             }
         }
-        throw new IllegalArgumentException("Source timestamp column '" + sourceTimestampColumnName + "' not found");
+        throw new IllegalArgumentException("Source timestamp column '" + tsColName + "' not found");
     }
 
-    private static void sendLine(LowerCaseCharSequenceHashSet symbols, ResultSet resultSet, int columnCount, String[] columnNames, int[] columnTypes, int timestampIndex, Sender sender, String tableName) throws SQLException {
+    private static void sendLine(LowerCaseCharSequenceHashSet symbols, ResultSet rs, int colCount,
+                                 String[] colNames, int[] colTypes, int tsIndex, Sender sender, String tableName) throws SQLException {
         sender.table(tableName);
-        for (int i = 0; i < columnCount; i++) {
-            // symbols first
-            String columnName = columnNames[i];
-            if (symbols.contains(columnName)) {
-                String value = resultSet.getString(i + 1);
+        for (int i = 0; i < colCount; i++) {
+            String colName = colNames[i];
+            if (symbols.contains(colName)) {
+                String value = rs.getString(i + 1);
                 if (value != null) {
-                    sender.symbol(columnName, value);
+                    sender.symbol(colName, value);
                 }
             }
         }
-
-        for (int i = 0; i < columnCount; i++) {
-            // symbols first
-            String columnName = columnNames[i];
-            if (!symbols.contains(columnName)) {
-                switch (columnTypes[i]) {
+        for (int i = 0; i < colCount; i++) {
+            String colName = colNames[i];
+            if (!symbols.contains(colName)) {
+                switch (colTypes[i]) {
                     case Types.INTEGER:
                     case Types.BIGINT:
                     case Types.SMALLINT:
                     case Types.TINYINT:
-                        long aLong = resultSet.getLong(i + 1);
-                        if (!resultSet.wasNull()) {
-                            sender.longColumn(columnName, aLong);
+                        long valLong = rs.getLong(i + 1);
+                        if (!rs.wasNull()) {
+                            sender.longColumn(colName, valLong);
                         }
                         break;
-
                     case Types.DOUBLE:
                     case Types.FLOAT:
                     case Types.REAL:
-                        double aDouble = resultSet.getDouble(i + 1);
-                        if (!resultSet.wasNull() && !Double.isNaN(aDouble)) {
-                            sender.doubleColumn(columnName, aDouble);
+                        double valDouble = rs.getDouble(i + 1);
+                        if (!rs.wasNull() && !Double.isNaN(valDouble)) {
+                            sender.doubleColumn(colName, valDouble);
                         }
                         break;
-
                     case Types.BOOLEAN:
                     case Types.BIT:
-                        boolean aBoolean = resultSet.getBoolean(i + 1);
-                        if (!resultSet.wasNull()) {
-                            sender.boolColumn(columnName, aBoolean);
+                        boolean valBoolean = rs.getBoolean(i + 1);
+                        if (!rs.wasNull()) {
+                            sender.boolColumn(colName, valBoolean);
                         }
                         break;
-
                     case Types.DATE:
                     case Types.TIMESTAMP:
-                        if (i != timestampIndex) {
-                            long microEpoch = getMicroEpoch(resultSet, i + 1);
-                            if (microEpoch != Numbers.LONG_NULL && !resultSet.wasNull()) {
-                                sender.timestampColumn(columnName, microEpoch, ChronoUnit.MICROS);
+                        if (i != tsIndex) {
+                            long tsMicroEpoch = getMicroEpoch(rs, i + 1);
+                            if (tsMicroEpoch != Long.MIN_VALUE && !rs.wasNull()) {
+                                sender.timestampColumn(colName, tsMicroEpoch, ChronoUnit.MICROS);
                             }
                         }
                         break;
-
                     case Types.CHAR:
                     case Types.VARCHAR:
                     case Types.LONGVARCHAR:
-                        String value = resultSet.getString(i + 1);
-                        if (value != null && !resultSet.wasNull()) {
-                            sender.stringColumn(columnName, resultSet.getString(i + 1));
+                        String valString = rs.getString(i + 1);
+                        if (valString != null && !rs.wasNull()) {
+                            sender.stringColumn(colName, valString);
                         }
                         break;
-
                     default:
-                        throw new UnsupportedOperationException("Unsupported column type: " + columnName);
+                        throw new UnsupportedOperationException("Unsupported column type: " + colName);
                 }
             }
         }
-
-        long microEpoch = getMicroEpoch(resultSet, timestampIndex + 1);
-        sender.at(microEpoch, ChronoUnit.MICROS);
+        long tsMicroEpoch = getMicroEpoch(rs, tsIndex + 1);
+        sender.at(tsMicroEpoch, ChronoUnit.MICROS);
     }
 }
